ae863e8a5c175165939a477a70b4f50904f904e3
Qanat89 <kalekeevkanat@mail.com>
2025-04-03 20:52:47 +0600

    init2

--- market-react/market/src/components/ui/Butoon.jsx
+++ market-react/market/src/components/ui/Butoon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Butoon({width, title, className, icon}) {
   return (
-  <button className={`${className} hover:bg-[#FF6633] cursor-pointer px-4 py-2 rounded-sm hover:text-white bg-transparent border-2 hover:border-[#FF6633] border-green-400 transition delay-100 duration-300 text-green-400 flex flex-row justify-center items-center ${width==="sm" ? `w-24`: width==="md"?"w-48" : width==="lg" ? "w-60": "w-24" }` }>
+  <button className={`${className} relative hover:bg-[#FF6633] cursor-pointer px-4 py-2 rounded-sm hover:text-white bg-transparent border-2 hover:border-[#FF6633] border-green-400 transition delay-100 duration-300 text-green-400 flex flex-row justify-center items-center ${width==="sm" ? `w-24`: width==="md"?"w-48" : width==="lg" ? "w-60": "w-24" }` }>
  <i>{icon}</i>
     {title}
   </button>
